f330ad030f29224064191abf22440ec982f6a2b5
Yung Bill$ <billy.waite91@gmail.com>
2016-08-30 07:54:11 -0400

    Updated the React Part of the Previewer

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-var Markedup = React.createClass({
-
+var InputDisplay = React.createClass({
   getInitialState: function() {
     return {
-      value: "Hello"
+      value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n',
     };
   },
   handleChange: function(event) {
@@ -10,15 +9,31 @@ var Markedup = React.createClass({
       value: event.target.value
     });
   },
-
   render: function() {
     return (
-      <div className="markedup">
-        <h1>{this.state.value}</h1>
-        <input type="text" placeholder="Type here" onChange={this.handleChange}/>
+      <div className="row">
+        <div className="small-5 small-offset-1 columns">
+          <textarea rows="25" value={this.state.value} onChange={this.handleChange}></textarea>
+        </div>
+        <div className="small-5 small-offset-1 columns">
+          <MarkupDisplay display={this.state.value} />
+        </div>
       </div>
     );
   }
 });
 
-ReactDOM.render(<Markedup/>, document.getElementById('mount-point'));
+var MarkupDisplay = React.createClass({
+    markup: function() {
+      var markup = marked(this.props.display, {sanitize: true});
+      return {__html: markup};
+    },
+    render: function() {
+      return (
+        <div dangerouslySetInnerHTML={this.markup()}>
+      </div>
+      );
+    }
+});
+
+ReactDOM.render(<InputDisplay/>, document.getElementById('mount-point'));
